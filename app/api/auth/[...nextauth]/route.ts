f4e674d21d4f1b9f985f974ca22b25b6429b1f60
SAIFUL ISLAM <SAIFUL ISLAM>
2025-11-25 11:47:23 +0600

    admin auth Done!

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -54,7 +54,7 @@ export const authOptions: NextAuthOptions = {
             name: user.name,
             email: user.email,
             image: user.image,
-            role: "user",
+            role: user.role,
             provider: "google",
             password: null, // Important
           });
