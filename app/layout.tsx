6516f73205b08f21c8cd46d6d53d43dabb1fe69d
SAIFUL ISLAM <SAIFUL ISLAM>
2025-12-08 13:18:07 +0600

    meta title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import WhatsApp from "@/components/WhatsApp";
 import Providers from "./providers"; // <-- create a wrapper for SessionProvider
 
 export const metadata: Metadata = {
-  title: "My App",
-  description: "Example Next.js app",
+  title: "Ravaa Fashion",
+  description: "The best fashion store in Dhaka Bangladesh",
 };
 
 export default function RootLayout({
